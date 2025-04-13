7d7ab414965ed14380490c2d3c4625f79e31cfac
Csontos Kincső <doomhyenaofficial@gmail.com>
2025-04-13 11:29:32 +0200

    Admin Törlégomb a főoldalon

--- index.php
+++ index.php
@@ -44,30 +44,34 @@
     </nav>
     <div>
         <?php
-            echo "<h2>Üdv ". $user['firstname'] ." a NoteShare oldalán!</h2>";
-        ?>
-        <h2>Itt megoszthatod és letöltheted az iskolai jegyzeteket.</h2>
-        <h3>Jegyzetek:</h3>
-        <?php
+            echo "<h1>Üdv ". $user['firstname'] ." a NoteShare oldalán!</h1>";
+            echo "<h2>Itt megoszthatod és letöltheted az iskolai jegyzeteket.</h2>";
+            echo "<h3>Feltöltött fájlok:</h3>";
             $sql = "SELECT files.*, users.username FROM files INNER JOIN users ON files.userid = users.id ORDER BY files.id DESC";
             $result = $conn->query($sql);
 
             if ($result->num_rows > 0) {
-            while ($file = $result->fetch_assoc()) {
-                echo "<div>";
-                if (!empty($file)) {
-                $folder = getcwd();
-                echo "<div>";
-                echo "<h4>" . $file['name'] . "</h4>";
-                echo "<iframe src='roles/users/" . $file['username'] . "/" . $file['tn_name'] . "' width='600' height='400'></iframe>";
-                echo "<a href='assets/php/download.php?id=" . $file['id'] . "'>Letöltés</a>";
-                echo "<p>Feltöltötte: <a href='profile.php?id=" . $file['userid'] . "'>" . $file['username'] . "</a></p>";
-                echo "</div>";
-                } else {
-                echo "<p>Nem található a fájl!</p>";
+                while ($file = $result->fetch_assoc()) {
+                    echo "<div>";
+                    if (!empty($file)) {
+                    $folder = getcwd();
+                    echo "<div>";
+                    echo "<h4>" . $file['name'] . "</h4>";
+                    echo "<iframe src='roles/users/" . $file['username'] . "/" . $file['tn_name'] . "' width='600' height='400'></iframe>";
+                    echo "<a href='assets/php/download.php?id=" . $file['id'] . "'>Letöltés</a>";
+                    echo "<p>Feltöltötte: <a href='profile.php?id=" . $file['userid'] . "'>" . $file['username'] . "</a></p>";                
+                    if ($user['admin'] == 1) {
+                        echo "<form method='POST' action='assets/php/delete.php'>";
+                        echo "<input type='hidden' name='file_id' value='" . $file['id'] . "'>";
+                        echo "<button type='submit'>Törlés</button>";
+                        echo "</form>";
+                    }
+                    echo "</div>";
+                    } else {
+                        echo "<p>Nem található a fájl!</p>";
+                    }
+                    echo "</div>";
                 }
-                echo "</div>";
-            }
             } else {
             echo "<p>Nincsenek feltöltött fájlok.</p>";
             }
