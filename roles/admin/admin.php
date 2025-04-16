5302b600ef8c88dc911c7565740cdfd3f9d3a691
Csontos Kincső <doomhyenaofficial@gmail.com>
2025-04-16 11:09:52 +0200

    Automatikus commit: Wed 04/16/2025 11:09:52.81

--- roles/admin/admin.php
+++ roles/admin/admin.php
@@ -45,6 +45,11 @@
             echo "<script>alert('Hiba: A felhasználónév mező üres.')</script>";
         }
     }
+
+    if(isset($_POST['removeuser-btn'])) {
+        $username = $_POST['username'];
+        $sql = "SELECT * FROM users WHERE id='" . $_COOKIE['id'] . "'";
+    }
 ?>
 <!DOCTYPE html>
 <html lang="hu">
@@ -61,19 +66,19 @@
     <body>
     <nav>
         <ul>
-            <li><a href="/index.php">Főoldal</a></li> 
-            <li><a href="/upload.php">Feltöltés</a></li> 
-            <li><a href="/myprofile.php">Profilom</a></li> 
-            <li><a href="/search.php">Keresés</a></li>
+            <li><a href="../../index.php">Főoldal</a></li> 
+            <li><a href="../../upload.php">Feltöltés</a></li> 
+            <li><a href="../../myprofile.php">Profilom</a></li> 
+            <li><a href="../../search.php">Keresés</a></li>
             <?php
             if ($user['admin'] == 1) {
-                echo '<li><a href="/roles/admin/admin.php">Admin</a></li>';
+                echo '<li><a href="admin.php">Admin</a></li>';
             }
             if ($user['teacher'] == 1) {
                 echo '<li><a href="/roles/teacher/teacher.php">Tanári felület</a></li>';
             }
             ?>
-            <li><a href="/assets/php/logout.php">Kijelentkezés</a></li> 
+            <li><a href="../../assets/php/logout.php">Kijelentkezés</a></li> 
         </ul>
     </nav>
     <h1>Admin Panel</h1>
@@ -92,6 +97,15 @@
             <label for="username">Felhasználónév:</label><br>
             <input type="text" id="username" name="username"><br><br>
             <input type="submit" name="removeadmin-btn" value="Admin eltávolítása">
+        </form>
+    </section>
+    <h2>felhasználók kezelése</h2>
+    <section>
+        <form method="post">
+            <label>Felhasználó törlése:</label><br>
+            <label for="username">Felhasználónév:</label><br>
+            <input type="text" id="username" name="username"><br><br>
+            <input type="submit" name="removeuser-btn" value="Felhasználó törlése">
         </form>
     </section>
     <script src='assets/js/script.js'></script>
