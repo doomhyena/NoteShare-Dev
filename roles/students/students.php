93a222ff592acacc2f5cc1b56309c03d2d88ff6c
Csontos Kincső <doomhyenaofficial@gmail.com>
2025-04-16 11:39:53 +0200

    Automatikus commit: Wed 04/16/2025 11:39:53.05

--- roles/students/students.php
+++ roles/students/students.php
@@ -7,25 +7,29 @@
        <meta name='keywords' content='iskola, jegyzet, megosztás, tanulás'>
        <meta name='author' content='Csontos Kincső, Szekeres Levente'>
        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+       <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico">
        <link rel='stylesheet' href='assets/css/styles.css'>
    </head>
    <body>
    <nav>
         <ul>
-            <li><a href="/index.php">Főoldal</a></li> 
-            <li><a href="/upload.php">Feltöltés</a></li> 
-            <li><a href="/myprofile.php">Profilom</a></li> 
-            <li><a href="/search.php">Keresés</a></li>
+            <li><a href="../../index.php">Főoldal</a></li> 
+            <li><a href="../../upload.php">Feltöltés</a></li> 
+            <li><a href="../../myprofile.php">Profilom</a></li> 
+            <li><a href="../../search.php">Keresés</a></li>
             <?php
-            if ($user['admin'] == 1) {
-                echo '<li><a href="">Admin</a></li>';
-            }
-            if ($user['teacher'] == 1) {
-                echo '<li><a href="teacher.php">Tanári felület</a></li>';
-            }
+                $sql = "SELECT * FROM users WHERE id='" . $_COOKIE['id'] . "'";
+                $found_user = $conn->query($sql);
+                $user = $found_user->fetch_assoc();
+
+                if ($user['admin'] == 1) {
+                    echo '<li><a href="admin.php">Admin</a></li>';
+                }
+                if ($user['teacher'] == 1) {
+                    echo '<li><a href="/roles/teacher/teacher.php">Tanári felület</a></li>';
+                }
             ?>
-            <li><a href="">Diák felület</a></li>
-            <li><a href="/assets/php/logout.php">Kijelentkezés</a></li> 
+            <li><a href="../../assets/php/logout.php">Kijelentkezés</a></li> 
         </ul>
     </nav>
    </body>
--- roles/teacher/teacher.php
+++ roles/teacher/teacher.php
@@ -60,37 +60,34 @@
        <meta name='keywords' content='iskola, jegyzet, megosztás, tanulás'>
        <meta name='author' content='Csontos Kincső, Szekeres Levente'>
        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+       <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico">
        <link rel='stylesheet' href='assets/css/styles.css'>
    </head>
    <body>
    <nav>
         <ul>
-            <li><a href="/index.php">Főoldal</a></li> 
-            <li><a href="/upload.php">Feltöltés</a></li> 
-            <li><a href="/myprofile.php">Profilom</a></li> 
-            <li><a href="/search.php">Keresés</a></li>
+            <li><a href="../../index.php">Főoldal</a></li> 
+            <li><a href="../../upload.php">Feltöltés</a></li> 
+            <li><a href="../../myprofile.php">Profilom</a></li> 
+            <li><a href="../../search.php">Keresés</a></li>
             <?php
-            if ($user['admin'] == 1) {
-                echo '<li><a href="">Admin</a></li>';
-            }
-            if ($user['teacher'] == 1) {
-                echo '<li><a href="teacher.php">Tanári felület</a></li>';
-            }
+                $sql = "SELECT * FROM users WHERE id='" . $_COOKIE['id'] . "'";
+                $found_user = $conn->query($sql);
+                $user = $found_user->fetch_assoc();
+
+                if ($user['admin'] == 1) {
+                    echo '<li><a href="../admin/admin.php">Admin</a></li>';
+                }
+                if ($user['teacher'] == 1) {
+                    echo '<li><a href="teacher/teacher.php">Tanári felület</a></li>';
+                }
             ?>
-            <li><a href="">Diák felület</a></li>
-            <li><a href="/assets/php/logout.php">Kijelentkezés</a></li> 
+            <li><a href="../../assets/php/logout.php">Kijelentkezés</a></li> 
         </ul>
     </nav>
     <div class="teacher-dashboard">
         <h1>Tanári Felület</h1>
         <p>Üdvözöljük a tanári oldalon, <?php echo htmlspecialchars($user['name']); ?>!</p>
-        <section class="teacher-communication">
-            <h2>Kommunikáció</h2>
-            <ul>
-            <li><a href="/roles/teacher/messages.php">Üzenetek</a></li>
-            <li><a href="/roles/teacher/announcements.php">Közlemények</a></li>
-            </ul>
-        </section>
         <section>
             <h2>Osztályok létrehozása</h2>
             <form method="POST">
