f8811965e0cce4e0f62b006647fb122b7e5410e4
Csontos Kincső <doomhyenaofficial@gmail.com>
2025-04-17 23:04:53 +0200

    Automatic commit: Thu 04/17/2025 23:04:53.44

--- assets/sql/noteshare.sql
+++ assets/sql/noteshare.sql
@@ -10,6 +10,7 @@ CREATE TABLE IF NOT EXISTS users (
     lastname VARCHAR(100),
     firstname VARCHAR(100),
     username VARCHAR(50) NOT NULL UNIQUE,
+    profile_picture VARCHAR(255),
     password VARCHAR(255) NOT NULL,
     security_question VARCHAR(255) NOT NULL,
     security_answer VARCHAR(255) NOT NULL,
@@ -86,6 +87,6 @@ INSERT INTO `files` (`id`, `uploaded_by`, `name`, `description`, `file_path`, `t
 (1, 2, 'C Jegyzetek', 'CNotesForProfessionals.pdf', 'C Jegyzetek', 'C:xampp	mpphpAD22.tmp'),
 (2, 1, 'CSS jegyzetek', 'CSSNotesForProfessionals.pdf', 'CSS jegyzetek', 'C:xampp	mpphp925D.tmp');
 
-INSERT INTO `users` (`id`, `lastname`, `firstname`, `username`, `profile_picture`, `password`, `teacher`, `admin`) VALUES
-(2, 'Csontos', 'Kincső', 'doomhyena', '618462_4xEbsnTA.png', '$2y$10$EwcPqq6Aw7/m39popdXq.uH45xjtV6knsEnKZ/gfJo/.dwXvp6Wzm', 0, 1),
-(1, 'Kelemen ', 'Boldizsár', 'tesztuser', '', '$2y$10$CI1lsAN6RWADb9L6otlv9eGxsTFAJ0H0KSdy9j.FT3IopoYSXbjBS', 0, 0);
+INSERT INTO `users` (`id`, `lastname`, `firstname`, `username`, `profile_picture`, `password`, `security_question`, `security_answer`, `teacher`, `admin`) VALUES
+(2, 'Csontos', 'Kincső', 'doomhyena', '618462_4xEbsnTA.png', '$2y$10$EwcPqq6Aw7/m39popdXq.uH45xjtV6knsEnKZ/gfJo/.dwXvp6Wzm', 'Mi a kedvenc könyved?', 'Ideológiák Tárháza', 0, 1),
+(1, 'Kelemen ', 'Boldizsár', 'tesztuser', '', '$2y$10$CI1lsAN6RWADb9L6otlv9eGxsTFAJ0H0KSdy9j.FT3IopoYSXbjBS', 'Mi a születési városod?', 'Budapest', 0, 0);
