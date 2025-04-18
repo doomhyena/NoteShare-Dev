862dd1f64bf267f15981cdb27c5d8580f415534e
Csontos Kincső <doomhyenaofficial@gmail.com>
2025-04-18 21:14:52 +0200

    Automatic commit: Fri 04/18/2025 21:14:52.47

--- assets/php/cfg.php
+++ assets/php/cfg.php
@@ -4,7 +4,8 @@
     $db_name = getenv('DB_NAME');
     $db_user = getenv('DB_USER');
     $db_pass = getenv('DB_PASS');
-    $conn = new mysqli("localhost", "root", "", "noteshare");
+    
+    $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
 
     if($conn->connect_error){
     die("Connection failed! ".$conn->connect_error);
