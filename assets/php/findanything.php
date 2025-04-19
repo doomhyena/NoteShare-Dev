1d3d41c371cbae13bc61e7732501a06e55049868
Csontos Kincső <doomhyenaofficial@gmail.com>
2025-04-19 04:49:53 +0200

    Automatic commit: Sat 04/19/2025  4:49:53.21

--- assets/php/findanything.php
+++ assets/php/findanything.php
@@ -12,4 +12,25 @@
 		echo "<a href='download.php?id=" . $curriculum['id'] . "'>Letöltés</a>";
 	}
 
+		
+	$sql = "SELECT * FROM users WHERE username LIKE '%$_GET[keresett]%' AND id != $_COOKIE[id]";
+	$founded_user = $conn->query($sql);
+	while($felhasznalo = $founded_user->fetch_assoc()){
+	
+		echo '<form class="user" method="post" action="search.php?userid='.$felhasznalo['id'].'">
+				<label>'.$felhasznalo['username'].'</label>';
+			
+			// Megvizsgáljuk, hogy a bejelentkezett felhasználó és a megtalált felhasználó között volt-e már barát kérelem
+			$sql = "SELECT * FROM friends WHERE fromid=$_COOKIE[id] AND toid=$felhasznalo[id] OR fromid=$felhasznalo[id] AND toid=$_COOKIE[id]";
+			$talalt_baratsag = $conn->query($sql);
+			
+			// Ha nem, akkor írjuk ki a jelölés gombot
+			if(mysqli_num_rows($talalt_baratsag) == 0){
+				echo '<input type="submit" name="add-friend-btn" value="Jelölés">';
+			}
+				
+		echo '</form>';
+	
+	}
+
 ?>
