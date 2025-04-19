66cc2a798913961218d5e64ba702e7775b027194
Csontos Kincső <doomhyenaofficial@gmail.com>
2025-04-19 04:54:53 +0200

    Automatic commit: Sat 04/19/2025  4:54:53.34

--- assets/php/findanything.php
+++ assets/php/findanything.php
@@ -1,36 +1,45 @@
-<?php 
+<?php
 
 	require "../../assets/php/cfg.php";
-	
-	$sql = "SELECT * FROM files WHERE name LIKE '%$_GET[keresett]%'";
-	$founded_curriculum = $conn->query($sql);
-	while($curriculum = $founded_curriculum->fetch_assoc()){
-	
-		echo '<form class="user" method="post" action="../../search.php?name='.$curriculum['name'].'">
-				<label>'.$curriculum['name'].'</label>';
-		echo '</form>';
-		echo "<a href='download.php?id=" . $curriculum['id'] . "'>Letöltés</a>";
+
+	$keresett = $_GET['keresett'] ?? '';
+	$loggedInUserId = $_COOKIE['id'] ?? 0;
+
+	$sqlFiles = "SELECT * FROM files WHERE name LIKE '%$keresett%'";
+	$resultFiles = $conn->query($sqlFiles);
+
+	while ($file = $resultFiles->fetch_assoc()) {
+		echo '
+			<form class="user" method="post" action="../../search.php?name=' . htmlspecialchars($file['name']) . '">
+				<label>' . htmlspecialchars($file['name']) . '</label>
+			</form>
+			<a href="download.php?id=' . (int)$file['id'] . '">Letöltés</a>
+		';
 	}
 
-		
-	$sql = "SELECT * FROM users WHERE username LIKE '%$_GET[keresett]%' AND id != $_COOKIE[id]";
-	$founded_user = $conn->query($sql);
-	while($felhasznalo = $founded_user->fetch_assoc()){
-	
-		echo '<form class="user" method="post" action="search.php?userid='.$felhasznalo['id'].'">
-				<label>'.$felhasznalo['username'].'</label>';
-			
-			// Megvizsgáljuk, hogy a bejelentkezett felhasználó és a megtalált felhasználó között volt-e már barát kérelem
-			$sql = "SELECT * FROM friends WHERE fromid=$_COOKIE[id] AND toid=$felhasznalo[id] OR fromid=$felhasznalo[id] AND toid=$_COOKIE[id]";
-			$talalt_baratsag = $conn->query($sql);
-			
-			// Ha nem, akkor írjuk ki a jelölés gombot
-			if(mysqli_num_rows($talalt_baratsag) == 0){
-				echo '<input type="submit" name="add-friend-btn" value="Jelölés">';
-			}
-				
+	$sqlUsers = "SELECT * FROM users WHERE username LIKE '%$keresett%' AND id != $loggedInUserId";
+	$resultUsers = $conn->query($sqlUsers);
+
+	while ($user = $resultUsers->fetch_assoc()) {
+		$userId = (int)$user['id'];
+
+		echo '
+			<form class="user" method="post" action="search.php?userid=' . $userId . '">
+				<label>' . htmlspecialchars($user['username']) . '</label>
+		';
+
+		$sqlFriendCheck = "
+			SELECT * FROM friends 
+			WHERE (fromid = $loggedInUserId AND toid = $userId) 
+			OR (fromid = $userId AND toid = $loggedInUserId)
+		";
+		$friendCheck = $conn->query($sqlFriendCheck);
+
+		if ($friendCheck->num_rows === 0) {
+			echo '<input type="submit" name="add-friend-btn" value="Jelölés">';
+		}
+
 		echo '</form>';
-	
 	}
 
 ?>
