bd1bae5f8be25ea41ba76608089ad932cd2dcbe4
Csontos Kincső <doomhyenaofficial@gmail.com>
2025-04-19 15:14:53 +0200

    Automatic commit: Sat 04/19/2025 15:14:53.57

--- index.php
+++ index.php
@@ -10,10 +10,6 @@
     $found_user = $conn->query($sql);
     $user = $found_user->fetch_assoc();
 
-    $sql = "SELECT * FROM notifys WHERE toid = $user[id] AND readed = 0";
-    $founded_notify = $conn->query($sql);
-    $notify_number = mysqli_num_rows($founded_notify);
-
 	if(isset($_POST['comment-btn'])){
 		
 		$postid = $_GET['postid'];
@@ -40,24 +36,20 @@
    <body>
    <nav>
         <ul>
+            <li><a href="index.php">Főoldal</a></li>
+            <li><a href="upload.php">Feltöltés</a></li>
+            <li><a href="profile.php">Profilom</a></li>
+            <li><a href="search.php">Keresés</a></li>
             <?php
-                echo '<li><a href="'.dirname(__FILE__).'/index.php">Főoldal</a></li>';
-                echo '<li><a href="'.dirname(__FILE__).'/upload.php">Feltöltés</a></li> ';
-                echo '<li><a href="'.dirname(__FILE__).'/profile.php">Profilom</a></li> ';
-                echo '<li><a href="'.dirname(__FILE__).'/search.php">Keresés</a></li>';
-                echo "<li><a href=".dirname(__FILE__)."/notify.php'>Értesítések ($ertesitesek_szama)</a></li>";
-                $sql = "SELECT * FROM users WHERE id='" . $_COOKIE['id'] . "'";
-                $found_user = $conn->query($sql);
-                $user = $found_user->fetch_assoc();
+                
+                $sql = "SELECT * FROM notifys WHERE toid = $user[id] AND readed = 0";
+                $founded_notify = $conn->query($sql);
+                $notify_number = mysqli_num_rows($founded_notify);
 
-                if ($user['admin'] == 1) {
-                    echo '<li><a href='.dirname(__FILE__).'/roles/admin/admin.php">Admin</a></li>';
-                }
-                if ($user['teacher'] == 1) {
-                    echo '<li><a href="'.dirname(__FILE__).'/roles/teacher/teacher.php">Tanári felület</a></li>';
-                }
-                echo '<li><a href="'.dirname(__FILE__).'/assets/php/logout.php">Kijelentkezés</a></li>';
+                echo "<li><a href=".dirname(__FILE__)."/notify.php'>Értesítések ($ertesitesek_szama)</a></li>";
+                
             ?>
+            <li><a href="/assets/php/logout.php">Kijelentkezés</a></li>
         </ul>
     </nav>
     <div>
--- notify.php
+++ notify.php
@@ -31,24 +31,20 @@
    <body>
    <nav>
         <ul>
+            <li><a href="'.dirname(__FILE__).'/index.php">Főoldal</a></li>
+            <li><a href="'.dirname(__FILE__).'/upload.php">Feltöltés</a></li>
+            <li><a href="'.dirname(__FILE__).'/profile.php">Profilom</a></li>
+            <li><a href="'.dirname(__FILE__).'/search.php">Keresés</a></li>
             <?php
-                echo '<li><a href="'.dirname(__FILE__).'/index.php">Főoldal</a></li>';
-                echo '<li><a href="'.dirname(__FILE__).'/upload.php">Feltöltés</a></li> ';
-                echo '<li><a href="'.dirname(__FILE__).'/profile.php">Profilom</a></li> ';
-                echo '<li><a href="'.dirname(__FILE__).'/search.php">Keresés</a></li>';
-                echo "<li><a href=".dirname(__FILE__)."/notify.php'>Értesítések ($ertesitesek_szama)</a></li>";
-                $sql = "SELECT * FROM users WHERE id='" . $_COOKIE['id'] . "'";
-                $found_user = $conn->query($sql);
-                $user = $found_user->fetch_assoc();
+                
+                $sql = "SELECT * FROM notifys WHERE toid = $user[id] AND readed = 0";
+                $founded_notify = $conn->query($sql);
+                $notify_number = mysqli_num_rows($founded_notify);
 
-                if ($user['admin'] == 1) {
-                    echo '<li><a href='.dirname(__FILE__).'/roles/admin/admin.php">Admin</a></li>';
-                }
-                if ($user['teacher'] == 1) {
-                    echo '<li><a href="'.dirname(__FILE__).'/roles/teacher/teacher.php">Tanári felület</a></li>';
-                }
-                echo '<li><a href="'.dirname(__FILE__).'/assets/php/logout.php">Kijelentkezés</a></li>';
+                echo "<li><a href=".dirname(__FILE__)."/notify.php'>Értesítések ($ertesitesek_szama)</a></li>";
+                
             ?>
+            <li><a href="'.dirname(__FILE__).'/assets/php/logout.php">Kijelentkezés</a></li>
         </ul>
     </nav>
     <?php
