1bed7741ca5fd35547f71712403d4f93588d1996
Csontos Kincső <doomhyenaofficial@gmail.com>
2025-04-20 14:44:53 +0200

    Automatic commit: Sun 04/20/2025 14:44:53.08

--- upload.php
+++ upload.php
@@ -61,7 +61,7 @@
                 <li><a href="index.php">Főoldal</a></li>
                 <li><a href="upload.php">Feltöltés</a></li>
                 <?php
-                    echo "<li><a href='profile.php?id=".$user['id']."'>Profilja</a></li>";
+                    echo "<li><a href='profile.php?userid=".$user['id']."'>Profilja</a></li>";
                 ?>
                 <li><a href="profile.php">Profilom</a></li>
                 <li><a href="search.php">Keresés</a></li>
