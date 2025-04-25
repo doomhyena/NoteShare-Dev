bdba17e74308cb295e8187071ac36c592c26c1d2
Csontos Kincső <doomhyenaofficial@gmail.com>
2025-04-25 17:04:53 +0200

    Automatic commit: Fri 04/25/2025 17:04:53.18

--- assets/php/download.php
+++ assets/php/download.php
@@ -1,31 +1,31 @@
 <?php
-    require "cfg.php";    
+    require "cfg.php";
+    
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
 
-    $sql = "SELECT * FROM users WHERE id = ?";
+    $sql = "SELECT * FROM users WHERE id='" . $_COOKIE['id'] . "'";
     $found_user = $conn->query($sql);
     $user = $found_user->fetch_assoc();
 
     if (isset($_GET['id'])) {
-        $file_id = intval($_GET['id']);
-        $sql = "SELECT * FROM files WHERE id = ?";
+        $file_id = $_GET['id'];
+        $sql = "SELECT * FROM files WHERE id='$file_id'";
         $result = $conn->query($sql);
 
         if ($result->num_rows > 0) {
             $file = $result->fetch_assoc();
             $file_name = $file['file_name'];
             $folder = getcwd();
-            $path = $folder . \\ . "users" . DIRECTORY_SEPARATOR . $user['username'] . DIRECTORY_SEPARATOR . $file_name;
+            $path = $folder."\\users\\".$user['username']."\\".$file_name;
 
             if (file_exists($path)) {
                 header('Content-Type: application/pdf');
                 header('Content-Disposition: attachment; filename="' . basename($path) . '"');
                 readfile($path);
-                exit;
-            } else {
-                echo "<script>alert('A fájl nem található!');</script>";
             }
         } else {
-            echo "<script>alert('A fájl nem található!');</script>";
+            echo "A fájl nem található.";
         }
     } else {
         echo "Nincs fájl kiválasztva.";
