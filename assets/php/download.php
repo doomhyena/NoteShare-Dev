b3c973fc288364e3d5a8a7e427dea2cf4c12c2bc
Csontos Kincső <doomhyenaofficial@gmail.com>
2025-04-25 17:24:53 +0200

    Automatic commit: Fri 04/25/2025 17:24:53.33

--- assets/php/download.php
+++ assets/php/download.php
@@ -7,15 +7,16 @@
         $file_result = $conn->query($sql);
 
         if ($file_result->num_rows > 0) {
-            $sql = "SELECT * FROM users WHERE id=" . $file_result->fetch_assoc()['uploaded_by'];
+            $file = $file_result->fetch_assoc(); // ELSŐ fetch_assoc egyszer!
+            
+            $sql = "SELECT * FROM users WHERE id=" . $file['uploaded_by'];
             $result = $conn->query($sql);
             $user = $result->fetch_assoc();
-
-            $file = $file_result->fetch_assoc();
-            $file_name = $file['file_name]; 
+        
+            $file_name = $file['file_name']; 
             $folder = getcwd();
-            $path = $folder."\\users\\".$user['username']."\\".$file_name;
-
+            $path = $folder . "\\users\\" . $user['username'] . "\\" . $file_name;
+        
             if (file_exists($path)) {
                 header('Content-Type: application/pdf');
                 header('Content-Disposition: attachment; filename="' . basename($path) . '"');
@@ -27,4 +28,5 @@
     } else {
         echo "Nincs fájl kiválasztva.";
     }
+
 ?>
