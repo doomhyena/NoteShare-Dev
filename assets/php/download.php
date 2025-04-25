fa77c9eda15599e95cf502ae0675bb8f33b504be
Csontos Kincső <doomhyenaofficial@gmail.com>
2025-04-25 17:19:53 +0200

    Automatic commit: Fri 04/25/2025 17:19:53.30

--- assets/php/download.php
+++ assets/php/download.php
@@ -4,16 +4,15 @@
     if (isset($_GET['id'])) {
         $file_id = $_GET['id'];
         $sql = "SELECT * FROM files WHERE id='$file_id'";
-        $result = $conn->query($sql);
+        $file_result = $conn->query($sql);
 
-        if ($result->num_rows > 0) {
-            $sql = "SELECT * FROM users WHERE id=" . $result->fetch_assoc()['uploaded_by'];
+        if ($file_result->num_rows > 0) {
+            $sql = "SELECT * FROM users WHERE id=" . $file_result->fetch_assoc()['uploaded_by'];
             $result = $conn->query($sql);
             $user = $result->fetch_assoc();
 
-            $sql = "SELECT * FROM files WHERE id='$file_id'";
-            $file = $result->fetch_assoc();
-            $file_name = $file['file_name'];
+            $file = $file_result->fetch_assoc();
+            $file_name = $file['file_name]; 
             $folder = getcwd();
             $path = $folder."\\users\\".$user['username']."\\".$file_name;
 
